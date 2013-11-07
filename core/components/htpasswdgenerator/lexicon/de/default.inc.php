6322ff1be41379d94c40b30e6a3994b416f0f938
christianseel <cs@chsmedien.de>
2013-11-07 13:10:50 +0100

    add readme and german lexicon file

---
+++ core/components/htpasswdgenerator/lexicon/de/default.inc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * de default topic lexicon file for htpasswdgenerator extra
+ *
+ * @package htpasswdgenerator
+ */
+
+$_lang['setting_htpasswd_auth_name'] = 'Auth Name';
+$_lang['setting_htpasswd_auth_name_desc'] = 'AuthName für .htaccess Datei. Wird auch als Überschrift für den Logindialog verwendet.';
+$_lang['setting_htpasswd_encrypt_type'] = 'Verschlüsselungsmethode';
+$_lang['setting_htpasswd_encrypt_type_desc'] = 'Bitte wählen Sie eine Verschlüsselungsmethode für die Passwörter. Sie haben die Wahl zwischen: "ENCTYPE_APR_MD5", "ENCTYPE_SHA1" oder "ENCTYPE_CRYPT"';
+$_lang['setting_htpasswd_commentline_first'] = 'htaccess Kommentar 1';
+$_lang['setting_htpasswd_commentline_first_desc'] = 'Muss mit einer Raute ("#") beginnen. Kommentarzeile, die die Pluginausgabe umfasst. Muss eindeutig sein.';
+$_lang['setting_htpasswd_commentline_last'] = 'htaccess Kommentar 2';
+$_lang['setting_htpasswd_commentline_last_desc'] = 'Muss mit einer Raute ("#") beginnen. Kommentarzeile, die die Pluginausgabe umfasst. Muss eindeutig sein.';
+$_lang['setting_htpasswd_base_path'] = 'Base Path';
+$_lang['setting_htpasswd_base_path_desc'] = 'Base Path für htaccess und htpasswd Dateien. Sollte in der Regel mit dem MODX Base Path übereinstimmen. Kann für Kontexte angepasst werden.';
